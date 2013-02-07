59a216a907624f198a385e179f102ae71bdba0bf
Vladimir Mencl <vladimir.mencl@canterbury.ac.nz>
2013-02-07 04:10:27 +0000

    fix df_non_browser_tools_tag in default config.php(2x)

--- df/usermgmt/dfpassword/config.php
+++ df/usermgmt/dfpassword/config.php
@@ -2,7 +2,7 @@
 
 $davis_properties_file = '/opt/davis/davis/webapps/root/WEB-INF/davis-host.properties';
 
-$df_non_browser_tools_tag = 'Accessing_the_DataFabric_in_anonymous_mode';
+$df_non_browser_tools_tag = 'Mounting_DataFabric_as_a_disk_drive';
 
 $log_file = '/var/www/html/dfpassword/.htirods/.htlog/passwordchange.log';
 $iCommandsPath = "/opt/iRODS/iRODS/clients/icommands/bin";
