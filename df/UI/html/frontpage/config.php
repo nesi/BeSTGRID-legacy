b33bee8afd4b5e7e0248404389aa4b6af8f3cd66
Vladimir Mencl <vladimir.mencl@canterbury.ac.nz>
2014-11-10 03:24:48 +0000

    DF landing page: add Otago DF node to default config

--- df/UI/html/frontpage/config.php
+++ df/UI/html/frontpage/config.php
@@ -2,7 +2,7 @@
 
 $davis_properties_file = '/opt/davis/davis/webapps/root/WEB-INF/davis-host.properties';
 
-$df_servers = "df.bestgrid.org:df.auckland.ac.nz";
+$df_servers = "df.bestgrid.org:df.auckland.ac.nz:df.uoo.nesi.org.nz";
 $geoip_path = "/opt/geoip/bin/geoip"
 
 ?>
