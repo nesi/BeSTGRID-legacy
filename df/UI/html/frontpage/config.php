5b8a81142e2f3e0ac837e72902e61239acd2f75c
Vladimir Mencl <vladimir.mencl@canterbury.ac.nz>
2014-11-20 22:02:45 +0000

    DF landing page: remove geoip_path from config now that geoip code has been removed from the landing page.

--- df/UI/html/frontpage/config.php
+++ df/UI/html/frontpage/config.php
@@ -3,7 +3,6 @@
 $davis_properties_file = '/opt/davis/davis/webapps/root/WEB-INF/davis-host.properties';
 
 $df_servers = "df.bestgrid.org:df.auckland.ac.nz:df.uoo.nesi.org.nz";
-$geoip_path = "/opt/geoip/bin/geoip";
 
 $df_announce = "";
 
